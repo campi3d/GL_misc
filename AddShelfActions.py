2ec9f8d8960c318ff6a1960af9e64597c12cf04f
Jens Kafitz <campi3d@users.noreply.github.com>
2018-06-02 19:08:45 +0200

    Generate Mari Shelf Files from Directories

--- AddShelfActions.py
+++ AddShelfActions.py
@@ -3,22 +3,6 @@ import os
 import json
 
 
-def saveMSH(data,jsonFilePath):
-    "stores a specifc project path for a given variable"
-
-
-    if os.path.exists(jsonFilePath):
-        with open(jsonFilePath, "r") as jsonFile:
-            try:
-                data = json.load(jsonFile)
-            except Exception:
-                pass
-
-    with open(jsonFilePath, "w+") as jsonFile:
-        jsonFile.write(json.dumps(data))
-
-
-
 
 def generateMSHFile():
     '''Generates a Mari Shelf File'''
@@ -30,20 +14,27 @@ def generateMSHFile():
     key = 0
 
     for dir in texdirs:
+        actionstring = ""
         absPath = srcdir + os.sep + dir
-        jsonFile = dir + '.msh'
-        jsonFilePath = absPath + os.sep + jsonFile
+        mshFile = dir + '.msh'
+        mshFilePath = absPath + os.sep + mshFile
         images = os.listdir(absPath)
-        beginningOfFile = '<!DOCTYPE MariML><Mari Version="2.1" FnVersion="4.1"> <MriActionCollection Type="MriActionCollection"><objectName Type="QString"></objectName><selectedType="bool">false</selected><uuidString Type="QString">{1b960a07-0529-415a-bf60-4524bdceb3f2}</uuidString><supportedTypes Type="QStringList"></supportedTypes><name Type="QString">Unnamed</name><m_ActionSources Type="QPair&lt; QString, QString>" ContainerType="QVector" Size="1">'
+        beginningOfFile = '<!DOCTYPE MariML><Mari Version="2.1" FnVersion="4.1"> <MriActionCollection Type="MriActionCollection"><objectName Type="QString"></objectName><selected Type="bool">false</selected><uuidString Type="QString">{1b960a07-0529-415a-bf60-4524bdceb3f2}</uuidString><supportedTypes Type="QStringList"></supportedTypes><name Type="QString">Unnamed</name><m_ActionSources Type="QPair&lt; QString, QString>" ContainerType="QVector" Size="1">'
+
         endofFile = '</m_ActionSources></MriActionCollection></Mari>'
 
         for img in images:
-            imgpath = os.path.join(absPath + os.sep + img)
-            actionstring = '<Action m_ActionSources_key="'+ str(key) + '" Type="QPair&lt; QString, QString>" Data="'+imgpath + '" ActionType="Mari/MriImagePath" Label="' + img + '"></Action>'
+            if img.endswith('msh'):
+                pass
+            else:
+                imgpath = os.path.join(absPath + os.sep + img)
+                actionstring = actionstring + '<Action m_ActionSources_key="' + str(key) + '" Data="'+ imgpath + '" ActionType="Mari/MriImagePath" Type="QPair&lt; QString, QString>" Label="' + img + '"></Action>'
 
         fileOutput = beginningOfFile + actionstring + endofFile
 
-        saveMSH(fileOutput,jsonFilePath)
+        with open(mshFilePath, 'w') as file:
+            file.write(fileOutput)
+            file.close()
 
 
 generateMSHFile()
